2a55d5bcefd4793b1f313e56920bbe4de610437d
Miguel <dev.miguellucena@gmail.com>
2023-03-13 00:09:41 +0100

    added Storybook configuration to operate with tailwindcss

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		{
+			name: '@storybook/addon-styling',
+			options: {
+				postCss: {
+					implementation: require("postcss"),
+				}
+			},
+		}
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: '@storybook/builder-vite',
+	},
+	features: {
+		storyStoreV7: true,
+	},
+};
